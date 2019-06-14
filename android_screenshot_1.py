cdc4452f8424cfcbeaaa1dd271a14aae9b750ef2
1051000378@qq.com <1051000378@qq.com>
2019-06-14 17:28:52 +0800

    android_screenshot_1 add log function

---
+++ android_screenshot_1.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*- #
+import datetime
+import os
+import subprocess
+import sys
+import time
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QGridLayout, QLineEdit
+
+__author__ = 'jiashumeng'
+'''
+Description: 
+'''
+
+'''
+1.执行adb命令获取设备
+2.截图保存到本地
+3.将图片pull到电脑上
+
+
+'''
+
+
+class Screenshot():  # 截取手机屏幕并保存到电脑
+    def __init__(self):
+        # 查看连接的手机
+        connect = subprocess.Popen("adb devices", stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        stdout, stderr = connect.communicate()  # 获取返回命令
+        # 输出执行命令结果结果
+        stdout = stdout.decode("utf-8")
+        stderr = stderr.decode("utf-8")
+        print(stdout)
+        print(stderr)
+
+    def screen(self, file_name):  # 在手机上截图
+        file_name_tem = self.get_file_name(".png").strip()
+        file_name1 = file_name.strip()
+        print(file_name1)
+        path = os.getcwd()
+        print(path)
+        cmd_screen = r"adb shell /system/bin/screencap -p /sdcard/" + file_name_tem  # 命令1：在手机上截图3.png为图片名
+        cmd_pull = r"adb pull /sdcard/" + file_name_tem + " " + path #不加strip()会有空格导致不能上传图片
+        print(cmd_screen)
+        screenExecute = subprocess.Popen(str(cmd_screen), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        stdout, stderr = screenExecute.communicate()
+        # 输出执行命令结果结果
+        stdout = stdout.decode("utf-8")
+        stderr = stderr.decode("utf-8")
+        print(stdout)
+        print(stderr)
+
+        screenExecute = subprocess.Popen(str(cmd_pull), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        stdout, stderr = screenExecute.communicate()
+        stdout = stdout.decode("utf-8")
+        stderr = stderr.decode("utf-8")
+        # 输出执行命令结果结果
+        # stdout = stdout.decode("utf-8")
+        # stderr = stderr.decode("utf-8")
+        print(stdout)
+        print(stderr)
+
+        screenExecute.terminate()
+
+        os.rename(os.path.join(path, file_name_tem), os.path.join(path, file_name1 + ".jpg"))
+
+
+    def get_file_name(self, name):
+        return datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + name
+
+
+    def screen_record(self, file_name):
+        # adb shell screenrecord /sdcard/123.mp4（保存到SDCard）
+        # adb pull /sdcard/123.mp4/ Users/uojie/Desktop/123.mp4（保存到电脑）
+        file_name_temp = self.get_file_name(".mp4").strip() # 两个file_name的原因： 如果输入框是中文，传输过程中就会出现找不到文件的问题，中文会转化成bytes，但是不知道该如何转化
+        file_name1 = file_name.strip()
+        print(file_name1)
+        path = os.getcwd()
+        print(path)
+        cmd1 = r"adb shell screenrecord --time-limit 10 /sdcard/" + file_name_temp # 命令1：在手机上截图3.png为图片名
+        cmd2 = r"adb pull /sdcard/" + file_name_temp + " " + path
+        print(cmd1)
+        print(cmd2)
+
+        screen_recored_Execute = subprocess.Popen(str(cmd1), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        stdout, stderr = screen_recored_Execute.communicate()
+        # 输出执行命令结果结果
+        stdout = stdout.decode("utf-8")
+        stderr = stderr.decode("utf-8")
+        print(stdout)
+        print(stderr)
+
+        screen_recored_Execute = subprocess.Popen(str(cmd2), stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        stdout, stderr = screen_recored_Execute.communicate()
+        stdout = stdout.decode("utf-8")
+        stderr = stderr.decode("utf-8")
+        print(stdout)
+        print(stderr)
+        screen_recored_Execute.terminate()   # 重点：adb 命令执行完后要关闭 terminate
+        os.rename(os.path.join(path, file_name_temp), os.path.join(path, file_name1 + ".mp4"))
+# filename = "C:/log.txt"
+#  logcat_file = open(filename, 'w')
+# logcmd = "adb logcat -v time"
+# self.Poplog = subprocess.Popen(logcmd,stdout=logcat_file,stderr=subprocess.PIPE)
+
+    def start_logcat(self,name):
+        log_name = name.strip() + '.txt'
+        self.logcat_file = open(log_name, 'w')
+        logcmd = "adb logcat -v time"
+        self.Poplog = subprocess.Popen(logcmd, stdout=self.logcat_file, stderr=subprocess.PIPE)
+
+    def stop_logcat(self):
+
+        self.logcat_file.close()
+        # subprocess.Popen("taskkill /F /T /PID " + str(self.Poplog.pid), shell=True)
+        self.Poplog.terminate()
+
+
+class ScreenUI(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.screen1 = Screenshot()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        layout = QGridLayout()
+        self.setWindowTitle("截图工具1.0")
+        # 这种静态的方法设置一个用于显示工具提示的字体。我们使用10px滑动字体
+        QToolTip.setFont(QFont('ScansSerif', 10))
+
+        #显示一个button按钮 截图控件
+        btn = QPushButton('截图', self)
+        btn.resize(btn.sizeHint()) # 设置默认尺寸
+        # btn.move(50, 50)
+        btn.clicked.connect(self.on_click)  # 监听点击事件
+
+        # 录制视频控件
+        btn_record = QPushButton("开始录制",self)
+        btn_record.resize(btn_record.sizeHint())
+        btn_record.clicked.connect(self.on_click1)
+
+        # todo 暂时没有想到可以停止录屏的功能，该功能暂时放弃
+
+        btn_start_logcat = QPushButton("抓取日志", self)
+        btn_start_logcat.resize(btn_start_logcat.sizeHint())
+        btn_start_logcat.clicked.connect(self.on_click2)
+
+        btn_stop_logcat = QPushButton("停止日志", self)
+        btn_stop_logcat.resize(btn_stop_logcat.sizeHint())
+        btn_stop_logcat.clicked.connect(self.on_click3)
+
+        # 输入框
+        self.edit_text = QLineEdit(" ")
+
+        layout.addWidget(self.edit_text, 1, 0)
+        layout.addWidget(btn, 2, 0)
+        layout.addWidget(btn_record, 3, 0)
+        layout.addWidget(btn_start_logcat, 4, 0)
+        layout.addWidget(btn_stop_logcat, 5, 0)
+
+
+        self.setLayout(layout)
+
+        self.show()
+
+    '''创建截图点击事件'''
+
+    def on_click(self):
+        name = self.edit_text.text()
+        print(name)
+        self.screen1.screen(name)
+
+    '''视频点击事件'''
+    def on_click1(self):
+        name = self.edit_text.text()
+        self.screen1.screen_record(name)
+
+    def on_click2(self):
+        name = self.edit_text.text()
+        self.screen1.start_logcat(name)
+
+    def on_click3(self):
+        self.screen1.stop_logcat()
+
+
+if __name__ == '__main__':
+    # os.system("adb devices")
+
+    # current_time = time.strftime('%Y-%m-%d-%H-%M-%S ', time.localtime(time.time()))+".png"
+    # cmd1 = r"adb shell /system/bin/screencap -p /sdcard/"+ current_time  # 命令1：在手机上截图3.png为图片名
+    # cmd2 = r"adb pull /sdcard/"+ current_time +" "+"D:\\123"  # 命令2：将图片保存到电脑 d:/3.png为要保存到电脑的路径
+
+    app = QApplication(sys.argv)
+    screen = ScreenUI()
+
+    sys.exit(app.exec_())
+
+
+
+
